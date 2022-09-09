c20f367f6776994a5897e48aa085c7171a56e1e4
babiswas2020 <bapan1680814@gmail.com>
2022-09-09 08:38:20 +0530

    commited

--- userapp/views.py
+++ userapp/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+def register_user(request):
+
+    '''Function based views to register user'''
+
+    pass
+
+def user_login(request):
+
+    '''Function based views for user login'''
+    pass
+
+def user_logout(request):
+
+    '''Function based views for user logout'''
+
+    pass
