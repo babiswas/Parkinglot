105223a28de7d3e07671b20c23358d56080c35bc
babiswas2020 <bapan1680814@gmail.com>
2022-09-09 21:24:30 +0530

    commited

--- parkinglot/views.py
+++ parkinglot/views.py
@@ -6,24 +6,6 @@ from .forms import TicketForm,LotForm,InactivateForm
 from django.http import HttpResponse
 
 
-def edit_lot(request,lotid):
-
-    '''Method to update the lot'''
-
-    instance=Lot.objects.get(pk=lotid)
-    if request.method == "POST":
-        form = LotForm(request.POST,instance=instance)
-        if form.is_valid():
-                if form.save():
-                    return redirect('caruser:login', permanent=True)
-                else:
-                    return render(request, 'parkinglot/edit_lot.html', {'form': form, 'message': messages})
-        else:
-                return render(request, 'parkinglot/edit_lot.html', {'form': form, 'message': messages})
-    form = LotForm(instance=instance)
-    return render(request, 'parkinglot/edit_lot.html', {'form': form})
-
-
 def lot_detail(request,lotid):
 
     '''Get the details of the lot'''
@@ -39,28 +21,6 @@ def all_lots(request):
     return render(request, 'parkinglot/all_lots.html', {'all_lots': all_lots })
 
 
-
-def add_ticket(request,lotid):
-
-    '''Method to create a ticket'''
-
-    lot = Lot.objects.get(pk=lotid)
-    cars= Car.objects.filter(carowner_id=request.user.id)
-    if lot.state=="AVAILABLE":
-        if request.method == "POST":
-            form = LotForm(request.POST)
-            if form.is_valid():
-                if form.save():
-                    return redirect('caruser:login', permanent=True)
-                else:
-                    return render(request, 'parkinglot/edit_lot.html', {'form': form, 'message': messages})
-            else:
-                return render(request, 'parkinglot/edit_lot.html', {'form': form, 'message': messages})
-    form = TicketForm()
-    form.fields[]
-    return render(request, 'parkinglot/lot_ticket.html', {'form': form ,'cars':cars})
-
-
 def add_ticket(request,lotid):
 
     '''Method to create a lot ticket'''
@@ -69,6 +29,8 @@ def add_ticket(request,lotid):
     cars= Car.objects.filter(carowner_id=request.user.id)
     if lot.state=="AVAILABLE":
             if request.method == "POST":
+                lot.state='OCCUPIED'
+                lot.save()
                 ticket=Ticket()
                 ticket.lot=lot
                 ticket.ticketowner=request.user
@@ -85,21 +47,16 @@ def inactivate_ticket(request,ticketid):
     '''Update the tickets using this service.'''
 
     ticket=Ticket.objects.get(pk=ticketid)
-    if lot.state == "AVAILABLE":
-        if request.method == "POST":
-            ticket = Ticket()
-            ticket.lot = lot
-            ticket.ticketowner = request.user
-            ticket.vehicle = request.POST.get('vehicle')
+    if request.method == "POST":
+            lotid=ticket.lot.id
+            lot=Lot.objects.get(pk=lotid)
+            lot.state='AVAILABLE'
+            lot.save()
+            ticket.ticketstate='INACTIVE'
             ticket.save()
             return redirect('parkinglot:success', permanent=True)
-        form = InactivateForm()
-        form.fields['vehicle'].queryset = cars
-        return render(request, 'parkinglot/lot_ticket.html', {'form': form})
-
-
-def delete_ticket(request):
-    pass
+    form = InactivateForm(instance=ticket)
+    return render(request, 'parkinglot/inactivate_ticket.html', {'form': form})
 
 
 def sucess(request):
